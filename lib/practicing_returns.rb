c372c44929766e5ffa68f26fc9a4099b47fdb766
Anthony DiPietrantonio <antdp425@aol.com>
2020-03-25 10:15:40 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -4,6 +4,7 @@ def hello(array)
   i = 0
   collection = []
   while i < array.length
+    #Pushes array[i] into collection on each pass through
     collection << yield(array[i])
     i += 1
   end
